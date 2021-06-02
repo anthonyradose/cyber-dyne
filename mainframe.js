81b5fc6e5124f3265a9401b5b57b2b3dcd22ba15
j86hughes <j86hughes@gmail.com>
2021-06-02 23:16:03 +0100

    fix: js and css cleanup mate

--- mainframe.js
+++ mainframe.js
@@ -1,29 +1,36 @@
 const input = document.querySelector('input');
-const btn = document.querySelector('add-task');
+const btn = document.querySelector('.addTask > button');
 
 btn.addEventListener('click', addList)
 
 function addList(e) {
-  console.log('YOHANS YALALALALALALALALALALALALALALALALALALALA');
   const notCompleted = document.querySelector('.not-done');
   const completed = document.querySelector('.done');
 
-  const newLi = createElement('li');
-  const checkBtn = createElement('button');
-  const delBtn = createElement('button');
+  const newLi = document.createElement('li');
+  const checkBtn = document.createElement('button');
+  const delBtn = document.createElement('button');
 
-  // checkBtn.innerHTML = '<i class="fa fa-check"></i>'
-  // delBtn.innerHTML = '<i class="fa fa-trash"></i>'
+  checkBtn.innerHTML = 'done'
+  delBtn.innerHTML = 'delete'
 
   if (input.value !== '') {
     newLi.textContent = input.value;
-    input.value = '';
     notCompleted.appendChild(newLi);
+    newLi.appendChild(checkBtn);
+    newLi.appendChild(delBtn);
   }
 
-  checkBtn.addEventListener('click', function() {
+  checkBtn.addEventListener('click', function () {
     const parent = this.parentNode;
     parent.remove();
-  })
+    completed.appendChild(parent);
+    checkBtn.style.display = 'none';
+  });
+
+  delBtn.addEventListener('click', function () {
+    const parent = this.parentNode;
+    parent.remove();
+  });
 
 };

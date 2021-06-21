65a115795f311567279301f847c1268f81421e08
j86hughes <j86hughes@gmail.com>
2021-06-22 00:03:21 +0100

    fix: render todo lists from arrays

--- mainframe.js
+++ mainframe.js
@@ -1,78 +1,87 @@
 const input = document.querySelector("input");
 const btn = document.querySelector(".addTask > button");
 
-const defaultList = [
+let notDoneList = [
   {
     text: "hack the mainframe tonight...",
+    id: 1,
   },
   {
     text: "Return X-Files season 2 tapes to Blockbuster",
+    id: 2,
   },
 ];
+
+let doneList = [];
 
 function populateList() {
   const notCompleted = document.querySelector(".not-done");
   const completed = document.querySelector(".done");
 
-  defaultList.forEach((item) => {
-    const newLi = document.createElement("li");
+  // Clear all items in the lists before rendering the updated lists
+  notCompleted.innerHTML = "";
+  completed.innerHTML = "";
+
+  notDoneList.forEach((item) => {
+    const newListItem = document.createElement("li");
     const checkBtn = document.createElement("button");
     const delBtn = document.createElement("button");
 
     checkBtn.innerHTML = "done";
     delBtn.innerHTML = "delete";
 
-    newLi.textContent = item.text;
-    newLi.appendChild(checkBtn);
-    newLi.appendChild(delBtn);
+    newListItem.textContent = item.text;
+    newListItem.appendChild(checkBtn);
+    newListItem.appendChild(delBtn);
 
+    // Mark item as done
     checkBtn.addEventListener("click", function () {
-      const parent = this.parentNode;
-      parent.remove();
-      completed.appendChild(parent);
-      checkBtn.style.display = "none";
-    });
-  
-    delBtn.addEventListener("click", function () {
-      const parent = this.parentNode;
-      parent.remove();
+      notDoneList = notDoneList.filter((element) => element.id !== item.id);
+      doneList.push(item);
+      populateList();
     });
 
-    notCompleted.appendChild(newLi);
+    // Delete item
+    delBtn.addEventListener("click", function () {
+      notDoneList = notDoneList.filter((element) => element.id !== item.id);
+      populateList();
+    });
+
+    notCompleted.appendChild(newListItem);
+  });
+
+  doneList.forEach((item) => {
+    const newListItem = document.createElement("li");
+    const delBtn = document.createElement("button");
+
+    delBtn.innerHTML = "delete";
+
+    newListItem.textContent = item.text;
+    newListItem.appendChild(delBtn);
+
+    // Delete item
+    delBtn.addEventListener("click", function () {
+      doneList = doneList.filter((element) => element.id !== item.id);
+      populateList();
+    });
+
+    completed.appendChild(newListItem);
   });
 }
+
+function addToList() {
+  if (input.value !== "") {
+    notDoneList.push({
+      text: input.value,
+      id: notDoneList.length + 1,
+    });
+
+    input.value = "";
+
+    populateList();
+  }
+}
+
+btn.addEventListener("click", addToList);
 
 populateList();
-
-btn.addEventListener("click", addList);
-
-function addList(e) {
-  const notCompleted = document.querySelector(".not-done");
-  const completed = document.querySelector(".done");
-
-  const newLi = document.createElement("li");
-  const checkBtn = document.createElement("button");
-  const delBtn = document.createElement("button");
-
-  checkBtn.innerHTML = "done";
-  delBtn.innerHTML = "delete";
-
-  if (input.value !== "") {
-    newLi.textContent = input.value;
-    notCompleted.appendChild(newLi);
-    newLi.appendChild(checkBtn);
-    newLi.appendChild(delBtn);
-  }
-
-  checkBtn.addEventListener("click", function () {
-    const parent = this.parentNode;
-    parent.remove();
-    completed.appendChild(parent);
-    checkBtn.style.display = "none";
-  });
-
-  delBtn.addEventListener("click", function () {
-    const parent = this.parentNode;
-    parent.remove();
-  });
-}
